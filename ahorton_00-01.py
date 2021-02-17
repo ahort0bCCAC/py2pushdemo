86c814deb3c94946e8dd5f25fc2d094f6c9bb313
Alissa <ahort0b@acd.ccac.edu>
2021-02-17 18:53:36 -0500

    demo push of Hello World code

---
+++ ahorton_00-01.py
@@ -0,0 +1,13 @@
+#DAT-119-NC01-20FA:ahorton_00-01.py
+"""
+Alissa Horton, DAT-119-NC01 (20FA)
+
+Introduction -- Hello World
+Due: 11:59pm on Friday, September 18 2020
+
+Question 1: Hello World
+
+"""
+
+print('Hello, world!')
+print('Programming is fun!')
